b5d1f208bb8c4b6b10791a3887aff4c25995d27c
Joao Paulo <joaopaulloojpos15@gmail.com>
2020-03-17 10:33:48 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CadastroPessoaComponent } from './cadastro/cadastro-pessoa/cadastro-pessoa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroPessoaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cadastro/cadastro-pessoa/cadastro-pessoa.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { Pessoa } from './models/Pessoa';
+
+@Component({
+  selector: 'app-cadastro-pessoa',
+  templateUrl: 'cadastro-pessoa.component.html',
+  styles: []
+})
+export class CadastroPessoaComponent implements OnInit {
+
+  pessoa: Pessoa = new Pessoa;
+  listaPessoa: Pessoa[] = []
+  totalPessoa: number = 0;
+  pessoaJovem: Pessoa = new Pessoa;
+  pessoaIdosa: Pessoa = new Pessoa;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    (document.getElementById('nome') as HTMLElement).focus();
+  }
+
+  random() {
+    return Math.floor(Math.random() * 1024);
+  }
+
+  passarParaCampoIdade(){
+    (document.getElementById('idade') as HTMLElement).focus();
+  }
+
+  ordenarLista() {
+    if ((this.listaPessoa.length) === 0) {
+      this.pessoaJovem = new Pessoa;
+      this.pessoaIdosa = new Pessoa;
+    }
+    
+    else {
+      this.listaPessoa.sort((a, b) => a.idade - b.idade);
+      this.pessoaJovem = this.listaPessoa[0];
+      this.pessoaIdosa = this.listaPessoa[this.listaPessoa.length - 1];
+    }
+  }
+
+  adicionar() {
+    if (this.pessoa.nome === undefined) {
+      return alert('O campo Nome não foi preenchido')
+    }
+    this.pessoa.id = this.random();
+    this.listaPessoa.push(this.pessoa);
+    this.totalPessoa += 1;
+    this.pessoa = new Pessoa;
+
+    (document.getElementById('nome') as HTMLElement).focus();
+    
+    this.ordenarLista();
+  }
+
+  excluir(id) {
+    let pessoaId = this.listaPessoa.findIndex(cd => cd.id === id);
+    this.listaPessoa.splice(pessoaId, 1);
+    this.totalPessoa -= 1;
+
+    this.ordenarLista()
+  }
+
+}
